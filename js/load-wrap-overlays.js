a0a15e01173c8a3a57528088c7cb9e968dc50fbc
ericsmith <software.ecs@gmail.com>
2016-01-20 17:55:42 -0800

    aklsdjfh

--- js/load-wrap-overlays.js
+++ js/load-wrap-overlays.js
@@ -3,14 +3,85 @@
 		if(index < 10)
 			index = "0" + index;
 		$( this ).html('<div class="2u 3u(xlarge) 4u(large) 6u(small) 12u(xsmall) wrap-details">'+
-		 '<h1>' + index + '</h1>' +
-		 '<div class="wrap-details-content">' + 
-		 '<ul><li><strong>Wrap Title</strong></li><li class="wrap-title">alksdjhfsalkj</li></ul>' +
-		 '<ul><li><strong>My Role</strong></li><li class="my-role">alksdjhfsalkj</li></ul>' +
-		 '<ul><li><strong>Wrap Type</strong></li><li class="wrap-type">alksdjhfsalkj</li></ul>' +
-		 '<p class="description"></p></div>' +
-		 '<a class="button">— Launch —</a></div>');
+			'<h1>' + index + '</h1>' +
+			'<div class="wrap-details-content">' + 
+			'<ul><li><strong>Wrap Title</strong></li><li class="title">alksdjhfsalkj</li></ul>' +
+			'<ul><li><strong>My Role</strong></li><li class="role">alksdjhfsalkj</li></ul>' +
+			'<ul><li><strong>Wrap Type</strong></li><li class="type">alksdjhfsalkj</li></ul>' +
+			'<p class="description"></p></div>' +
+			'<a class="button">—Launch—</a></div>');
 	});
-	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .wrap-title").text('Spotify Artist Spotlight');
-	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .my-role").text('Vis Design / Widget Design');
-	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .wrap-type").text('Widget Experimentation');
+	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .title").text('Spotify Artist Spotlight');
+	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .role").text('Vis Design / Widget Design');
+	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .type").text('Widget Experimentation');
+	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .description").text('Created as an experiment using the Spotify API');
+
+	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .title").text('Loverly Lookbook');
+	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
+	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .description").text('Created for one of our first client\'s. An example our our agency work.');
+
+	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .title").text('Baremade Bags Lookbook');
+	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
+	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .title").text('Nieman Marcus Valentino Lookbook');
+	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
+	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .title").text('Brunello Cucinelli Lookbook');
+	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
+	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .title").text('Tart Collections Lookbook');
+	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
+
+	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
+	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
+	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
