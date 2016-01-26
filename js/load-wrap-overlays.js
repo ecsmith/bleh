94d728d3a86186665cf5e1b6789a33d584d6c6ad
Eric Smith <esmith@wrap.co>
2016-01-26 00:29:43 -0800

    alkdsjfhas

--- js/load-wrap-overlays.js
+++ js/load-wrap-overlays.js
@@ -17,71 +17,71 @@
 	$( ".wrap-spotify .wrap-media-embed-overlay .wrap-details-content .description").text('Created as an experiment using the Spotify API');
 
 	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .title").text('Loverly Lookbook');
-	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
 	$( ".wrap-loverly .wrap-media-embed-overlay .wrap-details-content .description").text('Created for one of our first client\'s. An example our our agency work.');
 
 	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-bastard .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .title").text('Baremade Bags Lookbook');
-	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
 	$( ".wrap-bare .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .title").text('Nieman Marcus Valentino Lookbook');
-	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
 	$( ".wrap-valentino .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .title").text('Brunello Cucinelli Lookbook');
-	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .type").text('Client Wrap');
 	$( ".wrap-brunello .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-cascade .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .title").text('Tart Collections Lookbook');
-	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-tart .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-sportcore .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-raen .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-raen-loci .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-lincoln .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-statefarm .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-signup .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
 
 	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .title").text('Bearded Bastard');
-	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .role").text('Not Involved');
+	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .role").text('N/A');
 	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .type").text('Template Wrap');
 	$( ".wrap-camping .wrap-media-embed-overlay .wrap-details-content .description").text('Created as a Look & Feel template for Wrap.');
